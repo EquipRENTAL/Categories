48a3394a7076b13bfedba31080ea031a87f91f91
Jessica Son <jessicason18@gmail.com>
2018-07-22 20:01:19 -0700

    bulma

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -5,52 +5,67 @@ import sporting from './sporting.jpeg';
 import tools from './Tools.jpeg';
 import water from './water.jpeg';
 
-const Categories = props => 
+const Categories = props =>
 
 
-<div>
-    <div class ="container">   
-        <div>
-            <h2> Camping Gear </h2>
-        </div>
-        <div>
-            <img src={camping} alt="camping" className="images"/>
-            <ul>
-                <p class="description"> Item 1 </p>
-                <a href="/camping" target="_blank" class="description">browse more items</a>
-            </ul>
-        </div>
-        <div>
-            <h2> Sporting Goods </h2>
-        </div>
-        <div>
-            <img src={sporting} alt="sporting" class="images"/><br/>
-            <ul>
-                <p class="description"> Item 1 </p>
-                <a href="/sporting" target="_blank" class="description">browse more items</a>
-            </ul>
-        </div>
-        <div>
-            <h2> Nautical </h2>
-        </div>  
-        <div>
-            <img src={water} alt="water" class="images"/><br/>
-            <ul>
-                <p class="description"> Item 1 </p>
-                <a href="/water" target="_blank" class="description">browse more items</a>
-            </ul>
-        </div>
-        <div>
-            <h2> Tools and Misc. </h2>
-        </div>      
-        <div>
-            <img src={tools} alt="tools" class="images"/><br/>
-            <ul>
-                <p class="description"> Item 1 </p>
-                <a href="/tools" target="_blank" class="description">browse more items</a>
-            </ul>
+    <div>
+        <div class="container">
+
+            <div class="columns">
+                <div class="column">
+                    <div>
+                        <h2> Camping </h2>
+                    </div>
+                    <div>
+                        <img src={camping} alt="camping" className="images" />
+                        <ul>
+                            <p class="description"> Item 1 </p>
+                            <a href="/camping" target="_blank" class="description">browse more items</a>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div>
+                        <h2> Sporting Goods </h2>
+                    </div>
+                    <div>
+                        <img src={sporting} alt="sporting" class="images" /><br />
+                        <ul>
+                            <p class="description"> Item 1 </p>
+                            <a href="/sporting" target="_blank" class="description">browse more items</a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="columns">
+                <div class="column">
+                    <div>
+                        <h2> Nautical </h2>
+                    </div>
+                    <div>
+                        <img src={water} alt="water" class="images" /><br />
+                        <ul>
+                            <p class="description"> Item 1 </p>
+                            <a href="/water" target="_blank" class="description">browse more items</a>
+                        </ul>
+                    </div>
+                </div>
+                <div class="column">
+                    <div>
+                        <h2> Tools and Misc. </h2>
+                    </div>
+                    <div>
+                        <img src={tools} alt="tools" class="images" /><br />
+                        <ul>
+                            <p class="description"> Item 1 </p>
+                            <a href="/tools" target="_blank" class="description">browse more items</a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
- </div> 
 
 export default Categories;
